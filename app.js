6501ae2b2e9cd32686453f78adc890ab49a52ac5
caullens <caullens@ksu.edu>
2016-09-28 15:20:33 -0500

    Added States

---
+++ app.js
@@ -0,0 +1,115 @@
+"use strict";
+
+/* Classes */
+const Game = require('./game');
+
+/* Global variables */
+var canvas = document.getElementById('screen');
+var game = new Game(canvas, update, render);
+var image = new Image();
+image.src = 'assets/animals.png';
+
+// We have 9 pairs of possible cards that are about 212px square
+var cards = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+var board = [];
+while(cards.length > 0) {
+  var index = Math.floor(Math.random() * (cards.length - 1));
+  board.push({card: cards[index], flip: false});
+  cards.splice(index, 1);
+}
+
+var state = "waiting for click 1";
+var scores = [0,0];
+var player = 0;
+var card1, card2;
+
+console.log(board);
+
+// TODO: Place the cards on the board in random order
+
+canvas.onclick = function(event) {
+  event.preventDefault();
+  var x = Math.floor((event.clientX - 3) / 165);
+  var y = Math.floor((event.clientY  - 3) / 165);
+  var card = board[y*6+x];
+  if(!card || card.flip) return;
+  card.flip = true;
+  switch(state) {
+	  case "waiting for click 1":
+		card1 = card;
+		state = "waiting for click 2";
+		break;
+	  case "waiting for click 2":
+		state = "waiting for timer";
+		setTimeout(function(){
+		if(card.card == card1.card) {
+			scores[player]++;
+		}
+		else {
+			card.flip = false;
+			card1.flip = false;
+			//switch players
+			player = +!player;
+		}
+		break;
+		state = "waiting for click 1";
+		}, 3000);
+  }
+}
+
+/**
+ * @function masterLoop
+ * Advances the game in sync with the refresh rate of the screen
+ * @param {DOMHighResTimeStamp} timestamp the current time
+ */
+var masterLoop = function(timestamp) {
+  game.loop(timestamp);
+  window.requestAnimationFrame(masterLoop);
+}
+masterLoop(performance.now());
+
+
+/**
+ * @function update
+ * Updates the game state, moving
+ * game objects and handling interactions
+ * between them.
+ * @param {DOMHighResTimeStamp} elapsedTime indicates
+ * the number of milliseconds passed since the last frame.
+ */
+function update(elapsedTime) {
+
+}
+
+/**
+  * @function render
+  * Renders the current game state into a back buffer.
+  * @param {DOMHighResTimeStamp} elapsedTime indicates
+  * the number of milliseconds passed since the last frame.
+  * @param {CanvasRenderingContext2D} ctx the context to render to
+  */
+function render(elapsedTime, ctx) {
+  ctx.fillStyle = "#ff7777";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // TODO: Render the board
+  for(var y = 0; y < 3; y++) {
+    for(var x = 0; x < 6; x++) {
+      var card = board[y * 6 + x];
+      if(card.flip) {
+        // render cute animal
+        ctx.drawImage(image,
+          // Source rect
+          card.card % 3 * 212, Math.floor(card.card / 3) * 212, 212, 212,
+          // Dest rect
+          x * 165 + 3, y * 165 + 3, 160, 160
+        );
+      } else {
+        // draw the back of the card (212x212px)
+        ctx.fillStyle = "#3333ff";
+        ctx.fillRect(x * 165 + 3, y * 165 + 3, 160, 160);
+      }
+    }
+  }
+
+}
